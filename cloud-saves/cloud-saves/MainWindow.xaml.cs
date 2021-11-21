e1fedefbb9c2511aac521b088f659081326d3ae4
Christopher Mackay <cmackay732@gmail.com>
2021-11-21 14:08:44 -0500

    added xmlsettings

--- cloud-saves/cloud-saves/MainWindow.xaml.cs
+++ cloud-saves/cloud-saves/MainWindow.xaml.cs
@@ -12,17 +12,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace cloud_saves
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Specify settings and default values
+            //---------------------------------------------------------
+
+            Dictionary<string, string> settings = new Dictionary<string, string>();
+
+            settings.Add("LocalSave", "");
+            settings.Add("CloudSave", "");
+
+            XMLSettings.AppSettingsFile = "Settings.xml"; // Set the file path
+            XMLSettings.InitializeSettings(settings); // Create file and set default values
+
+            //----------------------------------------------------------
+
+            // Read settings values
+
+            txtLocal.Text = XMLSettings.GetSettingsValue("LocalSave");
+            txtCloud.Text = XMLSettings.GetSettingsValue("CloudSave");
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Set settings values
+
+            string local = txtLocal.Text;
+            string cloud = txtCloud.Text;
+
+            if (File.Exists(local) && File.Exists(cloud))
+            {
+                XMLSettings.SetSettingsValue("LocalSave", txtLocal.Text);
+                XMLSettings.SetSettingsValue("CloudSave", txtCloud.Text);
+                this.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
